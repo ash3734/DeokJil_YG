b0102c1c23b4df679217b96c3b7165f2b4b80cba
ash <ash3734@gmail.com>
2017-02-03 01:32:55 +0900

    홈화면 탭 오픈소스 레이아웃 적용 -ash

--- app/src/main/java/com/deokjilmate/www/deokjilmate/home/HomeActivity.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/home/HomeActivity.java
@@ -1,7 +1,9 @@
-package com.deokjilmate.www.deokjilmate;
+package com.deokjilmate.www.deokjilmate.home;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.deokjilmate.www.deokjilmate.R;
 
 public class HomeActivity extends AppCompatActivity {
 
