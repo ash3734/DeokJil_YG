81cad52b6da105f75af8744302bf2e15a61990ae
YoungGyuLee <sket8993@gmail.com>
2017-02-04 15:27:41 +0900

    Add FacebookLogin - YG

--- app/src/main/java/com/deokjilmate/www/deokjilmate/Login/LoginSelectActivity.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/Login/LoginSelectActivity.java
@@ -7,6 +7,14 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.deokjilmate.www.deokjilmate.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,6 +22,7 @@ import butterknife.OnClick;
 //여기는 로그인 방법 결정하는 곳(구글, 페북, 트위터, 커스텀)
 public class LoginSelectActivity extends AppCompatActivity {
 
+    private CallbackManager callbackManager;
 
     @BindView(R.id.LoginSelect_topImage)
     ImageView toobarImage;
@@ -24,6 +33,8 @@ public class LoginSelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_select);
         ButterKnife.bind(this);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
 
     //toolbar = (Toolbar)findViewById(R.id.)
         Glide.with(this).load(R.drawable.meta).into(toobarImage);
@@ -33,5 +44,40 @@ public class LoginSelectActivity extends AppCompatActivity {
     public void FindPwdEvent()
     {
         startActivity(new Intent(getApplicationContext(), FindPwdActivity.class));
+    }
+
+    @OnClick(R.id.LoginSelect_facebook)
+    public void LoginFacebook()//페이스북 로그인 버튼
+    {
+        LoginManager.getInstance().logInWithReadPermissions(LoginSelectActivity.this,
+                Arrays.asList("public_profile", "email"));
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+    @OnClick(R.id.LoginSelect_twitter)
+    public void LoginTwitter()//트위터 로그인 버튼
+    {
+
+    }
+    @OnClick(R.id.LoginSelect_google)
+    public void LoginGoogle()//구글 로그인 버튼
+    {
+
     }
 }
