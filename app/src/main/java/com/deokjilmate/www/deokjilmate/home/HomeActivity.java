3d4fb254ae5813df912d2dd2d88dafc59c28c8eb
ash <ash3734@gmail.com>
2017-02-04 20:48:09 +0900

    sorry....-ash

--- app/src/main/java/com/deokjilmate/www/deokjilmate/home/HomeActivity.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/home/HomeActivity.java
@@ -2,11 +2,13 @@ package com.deokjilmate.www.deokjilmate.home;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.deokjilmate.www.deokjilmate.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button helloWorld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
