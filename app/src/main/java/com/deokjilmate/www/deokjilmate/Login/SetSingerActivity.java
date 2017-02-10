24d6c1bc3e787dc596c45238d063fa3edd1629a1
YoungGyuLee <sket8993@gmail.com>
2017-02-11 02:27:41 +0900

    recyclerSupport resource in gradle, create Setsinger Activity(+ related class) - YG

--- app/src/main/java/com/deokjilmate/www/deokjilmate/Login/SetSingerActivity.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/Login/SetSingerActivity.java
@@ -11,5 +11,6 @@ public class SetSingerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_singer);
+
     }
 }
---
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/Login/SetSingerItemData.java
@@ -0,0 +1,23 @@
+package com.deokjilmate.www.deokjilmate.Login;
+
+/**
+ * Created by dldud on 2017-02-11.
+ */
+
+public class SetSingerItemData {
+    String singer_image;
+    // int singer;
+    //보통 이미지의 리소스는 인트로 저장.
+    String singer_name;
+    int singer_most;
+    //list에 있어야 할 것은 가수 이미지, 가수 이름, most설정 이미지(T/F)이 네 가지만 있으면 된다.
+
+
+
+    public SetSingerItemData(String singer_image,String singer_name, int singer_most) {
+        this.singer_image = singer_image;
+        // this.singer = singer;
+        this.singer_name = singer_name;
+        this.singer_most = singer_most;
+    }
+}
