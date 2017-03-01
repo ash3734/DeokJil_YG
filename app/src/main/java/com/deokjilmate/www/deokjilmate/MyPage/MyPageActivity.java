51a762cc31ca661a965895923f4b2664a9a85531
YoungGyuLee <sket8993@gmail.com>
2017-03-01 17:18:20 +0900

    change layout - YG2

--- app/src/main/java/com/deokjilmate/www/deokjilmate/MyPage/MyPageActivity.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/MyPage/MyPageActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
@@ -34,11 +35,19 @@ public class MyPageActivity extends AppCompatActivity {
     @BindView(R.id.MyPage_rankImage)
     ImageView rankImage;
 
+    @BindView(R.id.MyPage_mainSingerBackground)
+    ImageView backImage;
+
+    @BindView(R.id.MyPage_subRecycle)
+    RecyclerView recyclerView;
+
+    @BindView(R.id.MyPage_scrollView)
+    ScrollView scrollView;
+
     RequestManager requestManager_singer;
     RequestManager requestManager_rank;
     ArrayList<MyPageItemData> myPageItemDatas;
     LinearLayoutManager linearLayoutManager;
-    RecyclerView recyclerView;
     MyPageAdapter myPageAdapter;
 
     @Override
@@ -48,18 +57,29 @@ public class MyPageActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Glide.with(this).load(R.drawable.toolbar).into(toolbarImage);
         Glide.with(this).load(R.drawable.meta).into(plusSub);
+        Glide.with(this).load(R.drawable.meta).into(backImage);
 
         requestManager_singer = Glide.with(this);
         requestManager_rank = Glide.with(this);
 
-        recyclerView = (RecyclerView) findViewById(R.id.MyPage_subRecycle);
-        recyclerView.setHasFixedSize(true);
+        //recyclerView.setHasFixedSize(true);
         //recyclerView.get
         linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayout.VERTICAL);
         recyclerView.setLayoutManager(linearLayoutManager);
 
 
+
+
+        //서브
+        myPageItemDatas = new ArrayList<MyPageItemData>();
+        myPageItemDatas.add(new MyPageItemData(R.drawable.meta, R.drawable.meta, "aaaa", "1234"));
+        myPageItemDatas.add(new MyPageItemData(R.drawable.meta, R.drawable.meta, "bbbb", "3456"));
+
+
+        myPageAdapter = new MyPageAdapter(requestManager_singer, requestManager_rank, myPageItemDatas);
+
+        recyclerView.setAdapter(myPageAdapter);
 
 
 
@@ -70,14 +90,7 @@ public class MyPageActivity extends AppCompatActivity {
 
 
 
-        //서브
-        myPageItemDatas = new ArrayList<MyPageItemData>();
-        myPageItemDatas.add(new MyPageItemData(R.drawable.meta, R.drawable.meta, "aaaa", "1234"));
-        myPageItemDatas.add(new MyPageItemData(R.drawable.meta, R.drawable.meta, "bbbb", "3456"));
 
-        myPageAdapter = new MyPageAdapter(requestManager_singer, requestManager_rank, myPageItemDatas);
-
-        recyclerView.setAdapter(myPageAdapter);
 
 
     }
