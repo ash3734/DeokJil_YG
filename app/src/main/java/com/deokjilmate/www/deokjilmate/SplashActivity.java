38f471092a60167b768165be3863dfa5141c6acd
YoungGyuLee <sket8993@gmail.com>
2017-03-10 00:52:06 +0900

    EditLogin - YG2

--- app/src/main/java/com/deokjilmate/www/deokjilmate/SplashActivity.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/SplashActivity.java
@@ -5,22 +5,29 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
-import com.deokjilmate.www.deokjilmate.MyPage.MyPageActivity;
+import com.deokjilmate.www.deokjilmate.Login.LoginSelectActivity;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private static final String TWITTER_KEY = "ZPJ66hrJ8nJpMKRS297O9BgGQ ";
+    private static final String TWITTER_SECRET = "Kb41OH7xsJfEZ3Rzf9rCiBLE6mz47GMtiJBdCcbuYmtIDqCLHu";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable(){
             @Override
             public void run()
             {
-                startActivity(new Intent(getApplicationContext(), MyPageActivity.class));
+                startActivity(new Intent(getApplicationContext(), LoginSelectActivity.class));
                 finish();
             }
         }, 5000);
