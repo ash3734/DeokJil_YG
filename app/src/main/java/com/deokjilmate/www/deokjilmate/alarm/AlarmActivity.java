a42e9d115853328b74e801fc3f054f30cbd69ef6
ash <ash3734@gmail.com>
2017-03-20 20:46:20 +0900

    ??

--- app/src/main/java/com/deokjilmate/www/deokjilmate/alarm/AlarmActivity.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/alarm/AlarmActivity.java
@@ -26,7 +26,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class AlarmActivity extends AppCompatActivity implements MainView{
+public class AlarmActivity extends AppCompatActivity implements MainView {
 
     private ArrayList<String> reGroupList = null;
     private ArrayList<String> mGroupList = null;
@@ -58,15 +58,12 @@ public class AlarmActivity extends AppCompatActivity implements MainView{
     Button alarm_today_info;
 
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm_activity);
 
-        alarm_today_info = (Button)findViewById(R.id.alarm_today_info);
+        alarm_today_info = (Button) findViewById(R.id.alarm_today_info);
         alarm_today_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -107,14 +104,12 @@ public class AlarmActivity extends AppCompatActivity implements MainView{
 
 
         //layout을 가지고 와서 actionbar에 포팅을 시킵니다.
-View mCustomView = LayoutInflater.from(this).inflate(R.layout.layout_alarm_actionbar, null);
+        View mCustomView = LayoutInflater.from(this).inflate(R.layout.layout_alarm_actionbar, null);
         actionBar.setCustomView(mCustomView);
 
 
-
-
         // 액션바에 백그라운드 이미지를 아래처럼 입힐 수 있습니다. (drawable 폴더에 img_action_background.png 파일이 있어야 겠죠?)
-       // actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.main_topbar));
+        // actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.main_topbar));
 
         s_id = 0;
         m_id = 129;
@@ -122,10 +117,9 @@ View mCustomView = LayoutInflater.from(this).inflate(R.layout.layout_alarm_actio
         service = ApplicationController.getInstance().getNetworkService();
 
 
-        mListView = (ExpandableListView)findViewById(R.id.elv_list);
+        mListView = (ExpandableListView) findViewById(R.id.elv_list);
         mGroupList = new ArrayList<String>();
         mChildList = new ArrayList<ArrayList<ChildStateObject>>();
-
 
 
         Call<NoticeResult> getDetailData = service.getNoticeData(m_id);
@@ -136,36 +130,31 @@ View mCustomView = LayoutInflater.from(this).inflate(R.layout.layout_alarm_actio
             @Override
             public void onResponse(Call<NoticeResult> call, Response<NoticeResult> response) {
 
-                if(response.isSuccessful()){
+                if (response.isSuccessful()) {
 
-                    for (NoticeData data:response.body().result) {
+                    for (NoticeData data : response.body().result) {
                         if (!mGroupList.contains(data.name)) {
                             mGroupList.add(data.name);
                         }
                     }
 
 
-                    for(int i=0;i<mGroupList.size();i++){
+                    for (int i = 0; i < mGroupList.size(); i++) {
                         ArrayList<ChildStateObject> mChildListContent = new ArrayList<ChildStateObject>();
-                        for(int j=0;j<response.body().result.size();j++){
-                            if(mGroupList.get(i).equals(response.body().result.get(j).name)){
-                                if(response.body().result.get(j).mp_name.equals("쇼! 챔피언")&&response.body().result.get(j).notice.equals("t")){
-                                    mChildListContent.add(new ChildStateObject("쇼! 챔피언",true));
-                                }
-                                else if(response.body().result.get(j).mp_name.equals("쇼! 챔피언")&&response.body().result.get(j).notice.equals("f")){
-                                    mChildListContent.add(new ChildStateObject("쇼! 챔피언",false));
-                                }
-                                else if(response.body().result.get(j).mp_name.equals("엠카운트다운")&&response.body().result.get(j).notice.equals("t")){
-                                    mChildListContent.add(new ChildStateObject("엠카운트다운",true));
-                                }
-                                else if(response.body().result.get(j).mp_name.equals("엠카운트다운")&&response.body().result.get(j).notice.equals("f")){
-                                    mChildListContent.add(new ChildStateObject("엠카운트다운",false));
-                                }
-                                else if(response.body().result.get(j).mp_name.equals("인기가요")&&response.body().result.get(j).notice.equals("t")){
-                                    mChildListContent.add(new ChildStateObject("인기가요",true));
-                                }
-                                else if(response.body().result.get(j).mp_name.equals("인기가요")&&response.body().result.get(j).notice.equals("f")){
-                                    mChildListContent.add(new ChildStateObject("인기가요",false));
+                        for (int j = 0; j < response.body().result.size(); j++) {
+                            if (mGroupList.get(i).equals(response.body().result.get(j).name)) {
+                                if (response.body().result.get(j).mp_name.equals("쇼! 챔피언") && response.body().result.get(j).notice.equals("t")) {
+                                    mChildListContent.add(new ChildStateObject("쇼! 챔피언", true));
+                                } else if (response.body().result.get(j).mp_name.equals("쇼! 챔피언") && response.body().result.get(j).notice.equals("f")) {
+                                    mChildListContent.add(new ChildStateObject("쇼! 챔피언", false));
+                                } else if (response.body().result.get(j).mp_name.equals("엠카운트다운") && response.body().result.get(j).notice.equals("t")) {
+                                    mChildListContent.add(new ChildStateObject("엠카운트다운", true));
+                                } else if (response.body().result.get(j).mp_name.equals("엠카운트다운") && response.body().result.get(j).notice.equals("f")) {
+                                    mChildListContent.add(new ChildStateObject("엠카운트다운", false));
+                                } else if (response.body().result.get(j).mp_name.equals("인기가요") && response.body().result.get(j).notice.equals("t")) {
+                                    mChildListContent.add(new ChildStateObject("인기가요", true));
+                                } else if (response.body().result.get(j).mp_name.equals("인기가요") && response.body().result.get(j).notice.equals("f")) {
+                                    mChildListContent.add(new ChildStateObject("인기가요", false));
                                 }
                             }
                         }
@@ -176,6 +165,7 @@ View mCustomView = LayoutInflater.from(this).inflate(R.layout.layout_alarm_actio
                 }
 
             }
+
             @Override
             public void onFailure(Call<NoticeResult> call, Throwable t) {
 
@@ -183,13 +173,10 @@ View mCustomView = LayoutInflater.from(this).inflate(R.layout.layout_alarm_actio
         });
 
 
-
-        mBaseExpandableAdapter = new AlarmAdapter(this, mGroupList, mChildList,this);
-
+        mBaseExpandableAdapter = new AlarmAdapter(this, mGroupList, mChildList, this);
 
 
         mListView.setAdapter(mBaseExpandableAdapter);
-
 
 
         mListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
@@ -230,26 +217,22 @@ View mCustomView = LayoutInflater.from(this).inflate(R.layout.layout_alarm_actio
     }
 
 
-
-
     @Override
-    public void updateStateCheck(String sname, String voteName , boolean state) {
+    public void updateStateCheck(String sname, String voteName, boolean state) {
 
         int groupIndex = 0;
         int childIndex = 0;
 
-        for(int i = 0; i< mGroupList.size();i++)
-        {
-            if(mGroupList.get(i).equals(sname)){
+        for (int i = 0; i < mGroupList.size(); i++) {
+            if (mGroupList.get(i).equals(sname)) {
                 groupIndex = i;
                 break;
             }
         }
 
-        for(int j = 0 ; j<mChildList.get(groupIndex).size();j++){
+        for (int j = 0; j < mChildList.get(groupIndex).size(); j++) {
 
-            if(mChildList.get(groupIndex).get(j).name.equals(voteName))
-            {
+            if (mChildList.get(groupIndex).get(j).name.equals(voteName)) {
                 childIndex = j;
                 mChildList.get(groupIndex).get(j).state = state;
                 mBaseExpandableAdapter.notifyDataSetChanged();
@@ -258,40 +241,39 @@ View mCustomView = LayoutInflater.from(this).inflate(R.layout.layout_alarm_actio
         }
 
 
-        if(state)
-            requestName(sname,voteName, "t");
+        if (state)
+            requestName(sname, voteName, "t");
         else
-            requestName(sname,voteName, "f");
+            requestName(sname, voteName, "f");
 
     }
 
     @Override
-    public void requestName(String sname,String name,String notice) {
+    public void requestName(String sname, String name, String notice) {
 
         Log.i("myTag", String.valueOf(m_id));
         Log.i("myTag", String.valueOf(sname));
         Log.i("myTag", String.valueOf(name));
         Log.i("myTag", String.valueOf(notice));
 
-        retrofit2.Call<RegisterResult> requestRegister = service.requestRegister(new NoticeObject(m_id,sname,name,notice));
+        retrofit2.Call<RegisterResult> requestRegister = service.requestRegister(new NoticeObject(m_id, sname, name, notice));
         requestRegister.enqueue(new Callback<RegisterResult>() {
             @Override
             public void onResponse(retrofit2.Call<RegisterResult> call, Response<RegisterResult> response) {
-                if(response.isSuccessful()){
-                    if(response.body().result.equals("create")){
-                        Toast.makeText(getApplicationContext(),"성공",Toast.LENGTH_SHORT).show();
+                if (response.isSuccessful()) {
+                    if (response.body().result.equals("create")) {
+                        Toast.makeText(getApplicationContext(), "성공", Toast.LENGTH_SHORT).show();
                         finish();
                     }
-                }
- else{
-                    Toast.makeText(getApplicationContext(),"등록실패",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "등록실패", Toast.LENGTH_SHORT).show();
                 }
 
             }
 
             @Override
             public void onFailure(retrofit2.Call<RegisterResult> call, Throwable t) {
-                Toast.makeText(getApplicationContext(),"실패",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "실패", Toast.LENGTH_SHORT).show();
             }
         });
 
