f9e49aa25139c583b7ba3848b6a2d0d362ec5871
YG3 <2yg@iyeong-gyuui-MacBook-Pro.local>
2017-06-06 01:34:05 +0900

    Design - YG3

--- app/src/main/java/com/deokjilmate/www/deokjilmate/SplashActivity.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/SplashActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
-import com.deokjilmate.www.deokjilmate.Login.LoginSelectActivity;
+import com.deokjilmate.www.deokjilmate.Login.MainLoginActivity;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 
@@ -14,23 +14,29 @@ import io.fabric.sdk.android.Fabric;
 public class SplashActivity extends AppCompatActivity {
     private static final String TWITTER_KEY = "ZPJ66hrJ8nJpMKRS297O9BgGQ ";
     private static final String TWITTER_SECRET = "Kb41OH7xsJfEZ3Rzf9rCiBLE6mz47GMtiJBdCcbuYmtIDqCLHu";
+
+//    @BindView(R.id.Splash_background)
+//    ImageView background;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.splash);
+//                ButterKnife.bind(this);
+//                Glide.with(this).load(R.drawable.splash).into(background);
 
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
+                TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+                Fabric.with(this, new Twitter(authConfig));
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable(){
-            @Override
-            public void run()
-            {
-                startActivity(new Intent(getApplicationContext(), LoginSelectActivity.class));
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable(){
+                    @Override
+                    public void run()
+                    {
+                startActivity(new Intent(getApplicationContext(), MainLoginActivity.class));
                 finish();
             }
-        }, 5000);
+        }, 10000000);
     }
     //누구나 다 아는 그런 스플래쉬
 }
