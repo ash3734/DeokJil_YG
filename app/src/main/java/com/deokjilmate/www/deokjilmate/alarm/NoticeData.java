e55f5957d43178f2b41c62106ccd25932a008913
김민경-PC\김민경 <kimmk0924@gmail.com>
2017-08-07 11:53:36 +0900

    Revert "AlarmActivity save"
    This reverts commit ab6d315c1e14f4c019c05e5d193be1b6ff7b3196.

--- app/src/main/java/com/deokjilmate/www/deokjilmate/alarm/NoticeData.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/alarm/NoticeData.java
@@ -1,26 +1,16 @@
 package com.deokjilmate.www.deokjilmate.alarm;
 
 
-import java.util.ArrayList;
-
 public class NoticeData {
 
-    public String firebaseToken;
-    public int today_alarm; // 오늘의 알림 on,off -> 0,1
-    public int member_id;
-    public ArrayList<Integer> theshow_flag; // 더쇼
-    public ArrayList<Integer> mcount_flag; // 엠카
-    public ArrayList<Integer> ingi_flag; // 인기가요
-    public ArrayList<Integer> mcore_flag; // 쇼음악중심
+    public String name; // 가수이름
+    public String mp_name; // 방송이름
+    public String notice; // t,f 여부
 
 
-    public NoticeData(String firebaseToken, int today_alarm, int member_id, ArrayList<Integer> theshow_flag, ArrayList<Integer> mcount_flag, ArrayList<Integer> ingi_flag, ArrayList<Integer> mcore_flag) {
-        this.firebaseToken = firebaseToken;
-        this.today_alarm = today_alarm;
-        this.member_id = member_id;
-        this.theshow_flag = theshow_flag;
-        this.mcount_flag = mcount_flag;
-        this.ingi_flag = ingi_flag;
-        this.mcore_flag = mcore_flag;
+    public NoticeData(String name, String mp_name, String notice) {
+        this.name = name;
+        this.mp_name = mp_name;
+        this.notice = notice;
     }
 }
