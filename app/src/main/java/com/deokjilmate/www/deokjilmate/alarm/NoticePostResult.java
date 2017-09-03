be45c6e3fded335f22f9018731bc93a157b5a7b5
김민경-PC\김민경 <kimmk0924@gmail.com>
2017-09-03 20:37:14 +0900

    알람 완성

--- app/src/main/java/com/deokjilmate/www/deokjilmate/alarm/NoticePostResult.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/alarm/NoticePostResult.java
@@ -1,12 +1,11 @@
 package com.deokjilmate.www.deokjilmate.alarm;
 
-import java.util.ArrayList;
-
 /**
  * Created by 김민경 on 2017-08-20.
  */
 
 public class NoticePostResult {
 
-    ArrayList<NoticePostData> result;
+    boolean result;
+    String msg;
 }
