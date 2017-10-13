831a6e2cee47a0f8af66a0dd07b4ad5ff6804bc7
YG3 <2yg@iyeong-gyuui-MacBook-Pro.local>
2017-10-13 12:54:47 +0900

    알람 수정 - YG

--- app/src/main/java/com/deokjilmate/www/deokjilmate/alarm/NoticeData.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/alarm/NoticeData.java
@@ -5,14 +5,15 @@ import java.util.ArrayList;
 
 public class NoticeData {
 
-    ArrayList<Integer> zero_flag;
-    ArrayList<Integer> one_flag;
-    ArrayList<Integer> two_flag;
-    ArrayList<Integer> three_flag;
-    int today_alarm; // 오늘의 알림 on,off -> 0,1
+    ArrayList<ArrayList<Integer>> zero_flag;
+    ArrayList<ArrayList<Integer>> one_flag;
+    ArrayList<ArrayList<Integer>> two_flag;
+    ArrayList<ArrayList<Integer>> three_flag;
+    String today_alarm; // 오늘의 알림 on,off -> 0,1
 
 
-    public NoticeData(ArrayList<Integer> zero_flag, ArrayList<Integer> one_flag, ArrayList<Integer> two_flag, ArrayList<Integer> three_flag, int today_alarm) {
+    public NoticeData(ArrayList<ArrayList<Integer>> zero_flag, ArrayList<ArrayList<Integer>> one_flag,
+                      ArrayList<ArrayList<Integer>> two_flag, ArrayList<ArrayList<Integer>> three_flag, String today_alarm) {
         this.zero_flag = zero_flag;
         this.one_flag = one_flag;
         this.two_flag = two_flag;
