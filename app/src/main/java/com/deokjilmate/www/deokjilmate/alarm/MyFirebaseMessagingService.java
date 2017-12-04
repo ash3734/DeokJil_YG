e86341548ed3e599642cd864263c1b7a5fafc0e5
김민경-PC\김민경 <kimmk0924@gmail.com>
2017-12-04 23:26:36 +0900

    푸쉬 알림 조금 수정(아이콘, 내용 등등)

--- app/src/main/java/com/deokjilmate/www/deokjilmate/alarm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/deokjilmate/www/deokjilmate/alarm/MyFirebaseMessagingService.java
@@ -4,11 +4,12 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.deokjilmate.www.deokjilmate.R;
-import com.deokjilmate.www.deokjilmate.Setting.Inquiry.InquiryActivity;
+import com.deokjilmate.www.deokjilmate.home.HomeActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -35,16 +36,18 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void sendNotification(String body){
-        Intent intent = new Intent(this, InquiryActivity.class);
+        Intent intent = new Intent(this, HomeActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
         PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
 
         NotificationCompat.Builder notifiBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Firebase Cloud Messaging")
+                .setSmallIcon(R.drawable.launcher01)
+                .setContentTitle("덕질메이트")
                 .setContentText(body)
                 .setAutoCancel(true)
+                .setVibrate(new long[]{1, 1000})
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
                 .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
